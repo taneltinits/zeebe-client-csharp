e2d2153ee0c2f5a7d29f75a29eb225c211fcd060
tanel.tinits <tanel.tinits@bondora.com>
2024-05-08 09:33:43 +0300

    feature: example for using custom tokensupplier with Duende library

---
+++ Client.Cloud.TokenSupplier.Example/AspNetTokenSupplier.cs
@@ -0,0 +1,20 @@
+﻿using System.Security.Authentication;
+using Duende.AccessTokenManagement;
+using Zeebe.Client.Api.Builder;
+
+namespace Client.Cloud.TokenSupplier.Example;
+
+public class AspNetTokenSupplier(IClientCredentialsTokenManagementService service) : IAccessTokenSupplier
+{
+    public async Task<string> GetAccessTokenForRequestAsync(string authUri = null,
+        CancellationToken cancellationToken = new ())
+    {
+        var token = await service.GetAccessTokenAsync("zb-client", cancellationToken: cancellationToken);
+        if (token.AccessToken is null)
+        {
+            throw new AuthenticationException("Failed to get access token");
+        }
+
+        return token.AccessToken;
+    }
+}
---
+++ Client.Cloud.TokenSupplier.Example/Program.cs
@@ -0,0 +1,37 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Client.Cloud.TokenSupplier.Example;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using NLog.Extensions.Logging;
+using Zeebe.Client;
+using Zeebe.Client.Api.Builder;
+
+
+const string zeebeUrl = "https://localhost:26501";
+const string authUrl = "http://localhost:18080/auth/realms/camunda-platform/protocol/openid-connect/token";
+const string clientId = "zeebe";
+const string clientSecret = "zecret";
+
+HostApplicationBuilder builder = Host.CreateApplicationBuilder(args);
+builder.Services.AddDistributedMemoryCache();
+builder.Services.AddClientCredentialsTokenManagement().AddClient("zb-client", client =>
+{
+    client.TokenEndpoint = authUrl;
+    client.ClientId = clientId;
+    client.ClientSecret = clientSecret;
+});
+builder.Services.AddSingleton<IAccessTokenSupplier, AspNetTokenSupplier>();
+
+Console.WriteLine("Hello, World!");
+var client = ZeebeClient.Builder()
+    .UseLoggerFactory(new NLogLoggerFactory())
+    .UseGatewayAddress(zeebeUrl)
+    .UseTransportEncryption()
+    .UseAccessTokenSupplier(builder.Services.BuildServiceProvider().GetService<IAccessTokenSupplier>())
+    .Build();
+
+var topology = await client.TopologyRequest()
+    .Send();
+
+Console.WriteLine(topology);
